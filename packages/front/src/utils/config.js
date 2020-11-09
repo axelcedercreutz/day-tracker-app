adc019911f50dab645241d87e3bc5c0242ce6340
Axel Cedercreutz <axelcedercreutz@users.noreply.github.com>
2020-11-09 12:51:29 +0200

    FIXES

--- packages/front/src/utils/config.js
+++ packages/front/src/utils/config.js
@@ -1,7 +1,7 @@
 const PORT = 3003;
 const BASE_URL = window.location.origin.match('localhost')
   ? `http://localhost:${PORT}/`
-  : `https://day-tracker-app-backend.herokuapp.com:${PORT}`;
+  : `https://day-tracker-app-backend.herokuapp.com:${PORT}/`;
 
 module.exports = {
   BASE_URL,
